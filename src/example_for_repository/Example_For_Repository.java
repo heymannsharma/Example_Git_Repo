690f9cf69d08f95e9c5b8326ecd026fc16139c41
Heymann <heymannsharma7161@gmail.com>
2019-06-17 14:33:36 -0400

    This is my program for Division

---
+++ src/example_for_repository/Example_For_Repository.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example_for_repository;
+
+/**
+ *
+ * @author heymannsharma
+ */
+public class Example_For_Repository {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x, y;
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.println("enter the first number");
+        x = sc.nextInt();
+        System.out.println("enter the second number");
+        y = sc.nextInt();
+        int div = x / y;
+        System.out.println("Sum is " + div);
+
+    }
+
+}
